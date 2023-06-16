a370585296cda189c88f1541721ae3cb0ef595fa
Abd Elkebir HAKAM <ehakam@student.1337.ma>
2023-06-16 16:07:53 +0100

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://raw.githubusercontent.com/hakamdev/MOVE_TO_GOINFRE/main/move_to_goingfre.sh > $HOME/move_to_goinfre.sh;
+echo >> $HOME/.zshrc
+echo 'bash $HOME/move_to_goinfre.sh' >> $HOME/.zshrc
