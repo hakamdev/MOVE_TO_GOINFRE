7d487faf6657d1ecd7378479431a0a98cc1af1d3
Abd Elkebir HAKAM <ehakam@student.1337.ma>
2023-06-16 16:19:35 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,6 +1,11 @@
 curl -fsSL https://raw.githubusercontent.com/hakamdev/MOVE_TO_GOINFRE/main/move_to_goingfre.sh > $HOME/move_to_goinfre.sh;
-echo >> $HOME/.zshrc
-echo 'bash $HOME/move_to_goinfre.sh' >> $HOME/.zshrc
+
+ALREADY_ADDED=$(cat $HOME/.zshrc | grep move_to_goinfre.sh);
+
+if [ "$ALREADY_ADDED" = "" ]; then
+  echo >> $HOME/.zshrc
+  echo 'bash $HOME/move_to_goinfre.sh' >> $HOME/.zshrc
+fi
 
 echo "Well done! ✅"
 echo "Now, go back to the github page and do the last steps."
