0152bdd8b303dfd4a1c69096985d6a56445342a4
Abd Elkebir HAKAM <ehakam@student.1337.ma>
2023-06-17 03:14:05 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,4 +6,3 @@ rm -rf $HOME/Library/LaunchAgents/com.apple.Terminal.plist
 rm -rf $HOME/Library/Caches $HOME/Library/Application\ Support/Google $HOME/Library/Application\ Support/JetBrains $HOME/Library/Application\ Support/Mozilla $HOME/Library/Application\ Support/Postman $HOME/Library/Application\ Support/Spotify $HOME/Library/Application\ Support/iTerm2 $HOME/Library/Application\ Support/Code $HOME/Library/Application\ Support/Docker\ Desktop $HOME/Library/Application\ Support/BraveSoftware $HOME/Library/Application\ Support/Logitech $HOME/Library/Application\ Support/org.videolan.vlc $HOME/.vscode                                  
 
 echo "Done ✅"
-echo "Now, if you want the stop the terminal from opening every time you log in, open Settings > Login items and remove it from the list."
